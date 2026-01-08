d88c466e085b0032f11d5ce42c96e35b294526a3
Rob J Hyndman <robjhyndman@gmail.com>
2026-01-08 15:16:38 +1100

    Updated renv incl new oldfaithful data

--- examples/old_faithful.R
+++ examples/old_faithful.R
@@ -2,21 +2,15 @@ remotes::install_github("robjhyndman/weird-package")
 library(weird)
 
 oldfaithful |>
-  filter(duration > 7000)
-
-oldfaithful |>
-  filter(duration < 1000) |>
   ggplot(aes(x = duration)) +
   geom_boxplot()
 
 oldfaithful |>
-  filter(duration < 1000) |>
   ggplot(aes(x = duration)) +
   geom_density() +
   geom_rug()
 
 oldfaithful |>
-  filter(duration < 1000) |>
   ggplot(aes(x = duration, y = waiting)) +
   geom_point(alpha = 0.2) +
   geom_smooth()
